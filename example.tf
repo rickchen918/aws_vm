de5db281d55a15ae38419599923119f2becad1a8
rickchen <rickchen918@gmail.com>
2022-09-07 20:46:38 +0800

    update ip address in list

--- example.tf
+++ example.tf
@@ -2,7 +2,7 @@ module "fw-spoke1-testvm" {
   source = "./modules/aws_vm"
   vpc_id = module.spoke1.vpc.vpc_id
   name = "fw1-spoke-testvm"
-  ssh_source = ["220.132.86.183/32","192.168.0.1/32"]
+  ssh_source = ["192.168.0.0/32","192.168.0.1/32"]
   image_name = "ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-20220609"
   associate_public_ip_address = true
   key_name = "tokyo"
